9adf9539a12c493d248add0fcbbdf8149c3ca433
Tadjaur <wk.taur@gmail.com>
2023-04-06 07:51:59 +0100

    Revert "Stable (#9)" (#10)
    This reverts commit 2270c30445b2f00386075d285804e9bae48b7138.

--- src/deps/deps.ts
+++
@@ -1,9 +0,0 @@
-import * as https from 'https';
-import * as Axios from 'axios';
-export * as classValidator from 'class-validator';
-export { AxiosError, AxiosResponse, AxiosRequestConfig  } from 'axios';
-export const axios = Axios.default.create({
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false, // bypass issue: https://github.com/softwebos/orange_money_apis/issues/4
-  }),
-});
